5000e370a7a8f76ca81ebeccd404f56f91e5c5d6
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2022-08-08 21:23:57 -0400

    s11-c1 Modifier le titre h1 dans single.php

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php
 get_header(); ?>
 <section class="site__main">
-    <h1>Mon modèle Wordpress pour Tp1</h1>
+    <h1>Mon modèle Wordpress pour Exercice #04</h1>
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>

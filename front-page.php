85f8ba52ae2457cfbdd626091f74881de376bbee
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2022-08-09 17:03:46 -0400

    s11-c1 Modifier le contenu du fichier front-page.php afin d'afficher le titre, sous titre et le résumé d'un article

--- front-page.php
+++ front-page.php
@@ -1,23 +1,13 @@
 <?php
 get_header(); ?>
 <section class="site__main">
-    <h1> Mon modèle Wordpress pour TP1</h1>
+    <h1> Mon modèle Wordpress pour Exercice#4</h1>
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
-            <h2><?php the_title(); ?></h2>
-
-            <?php $lien = " ... <a class='bouton' href='"
-                . get_permalink()
-                . "'>"
-                . substr(get_the_title(), 0, 8)
-                . "</a>";
-
-            ?>
-
-            <p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
-
-
+            <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
+            <h3><?php the_field('sous_titre'); ?></h3>
+            <p><?php the_field('resume'); ?></p>
         <?php endwhile; ?>
     <?php endif; ?>
 </section>

f1c53bf9e8611e9deab892360ab3dfc3409d56f0
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2022-08-09 00:50:30 -0400

    s11-c1 Ajouter le fichier 404.php quand une page est introuvable

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Mon modèle par défaut 31w
+ *
+ 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package mon_31w
+ */
+get_header(); ?>
+<h1>ERREUR 404 ! Page introuvable :(</h1>
+
+
+<?php get_footer(); ?>

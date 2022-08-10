8f1c5040a39488fcc953edbb8bcd0eb7c5976b43
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2022-08-09 20:58:18 -0400

    s11-c1 Ajouter du code dans le fichier front-page.php pour afficher l'image d'un article

--- front-page.php
+++ front-page.php
@@ -6,6 +6,11 @@ get_header(); ?>
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
             <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
+            <?php
+            if (get_field('image')) : ?>
+                <img src="<?php the_field('image'); ?>" />
+            <?php endif; ?>
+
             <h3><?php the_field('sous_titre'); ?></h3>
             <p><?php the_field('resume'); ?></p>
         <?php endwhile; ?>

a6ccc69c5a57dc84f2ec87ece3e198e4ca6c2c8e
sadek <bilal2dellys@hotmail.com>
2022-08-12 21:30:29 -0400

    s11-c1 Modifier l'ordre des éléments dans un article

--- front-page.php
+++ front-page.php
@@ -6,12 +6,11 @@ get_header(); ?>
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
             <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
+            <h3><?php the_field('sous_titre'); ?></h3>
             <?php
             if (get_field('image')) : ?>
                 <img src="<?php the_field('image'); ?>" />
             <?php endif; ?>
-
-            <h3><?php the_field('sous_titre'); ?></h3>
             <p><?php the_field('resume'); ?></p>
         <?php endwhile; ?>
     <?php endif; ?>
